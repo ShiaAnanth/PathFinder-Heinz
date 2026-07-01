a20ae50ebe5b1ced1186ddf14ef8c3a44deedba3
ShiaAnanth <ananshia.ananth28@gmail.com>
2026-07-01 12:44:18 -0400

    switched to storing scrapped pdf content into markdown instead of storing in json format for cleaner structure and readability, extracted md text from originial pdfs, created a new directory and file to store the extracted texts

--- pdfscraper.py
+++ pdfscraper.py
@@ -1,34 +1,18 @@
-import pypdf
 import os
-import json
+import pymupdf4llm
 
 
-dir_path = 'data'
 
-handbook_texts = {}
-for file in os.scandir(dir_path): #os library
-    if file.name.endswith(".pdf"):
-        program_name = file.name.replace('.pdf', '').split('-')[0].upper()
-        with open(file, 'rb') as pdf_file: #rb stands for read only and binary so that the file does not convert non text elements
-                
-            reader = pypdf.PdfReader(pdf_file)
-            full_text = ""
-            for page in reader.pages: #loops through every page in the reader starting from page 3
-                new = (page.extract_text()) #extracts all the text from the page 
-                clean_page =' '.join(new.split())  #in a new empty variable, it adds the extracted text and also removes the spacing in the characters
-                full_text += " " + clean_page
-                
-            handbook_texts[program_name] = full_text  # store in dictionary
+dir_path = "data"
+out_path = "output"
+os.makedirs(out_path, exist_ok=True)  # makes the output path the variable "out_path" and does not crash if folder already exists on second run
+
+
+for handbooks in os.scandir(dir_path): #os library
+    if handbooks.name.endswith(".pdf"):
+            hb_md = pymupdf4llm.to_markdown(handbooks.path)  # extracts the markdown text from the handbooks original folder and stores it in variable new_hb_md
+            program_name = handbooks.name.replace(".pdf", "") #stores the name of the hanbook into a variable by removing the .pdf from the end of its file name
+            new_hb = os.path.join(out_path, f"{program_name}.md") #adds a new .md file to our output directory
+            with open(new_hb,"w", encoding="utf-8") as newfile: #opens the new_hb file created in the output directory as newfile to write in it
+                 newfile.write(hb_md) #writes the extracted markdown from the current handbook 
             
-# after the loop, print to verify it worked
-#for name, text in handbook_texts.items(): #looks through all keys in dictionary
-    #print(f"{name}: {text[:100]}")  # print first 100 chars of each
-
-# Save to file
-with open("handbooks.json", "w", encoding='utf-8') as hb_files:
-    json.dump(handbook_texts, hb_files, indent=4, sort_keys=True, ensure_ascii=False)
-
-
-
-
-
